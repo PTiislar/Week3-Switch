906a9328963e4ccfa32a11d72d221369de282c73
Pirgit Tiislar <ptiislar@tktk.ee>
2021-09-29 22:14:19 +0300

    add new projects

---
+++ Grading/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Grading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // programm kusib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvab "Suureparane!",
+            //'B', konsool kuvab "Vaga hea!".
+            //'C', konsool kuvab "Hea!",
+            //'D', konsool kuvab "Rahuldav!",
+            //'E', konsool kuvab "Kasin!",
+            //'F', konsool kuvab "Puudulik!",
+            // kui kasutaja siestab midagi muud, siis konsool kuvab "vale vaartus"
+
+            //kasitame IF
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            //char- character
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+            
+            if(userResult == 'A')
+            {
+                Console.WriteLine("Suurepärane!");
+            }
+            else if(userResult == 'B')
+            {
+                Console.WriteLine("Väga hea!");
+            }
+            else if(userResult == 'C')
+            {
+                Console.WriteLine("Hea!");
+            }
+            else if(userResult == 'D')
+            {
+                Console.WriteLine("Rahuldav!");
+            }
+            else if(userResult == 'E')
+            {
+                Console.WriteLine("Kasin!");
+            }
+            else if(userResult == 'F')
+            {
+                Console.WriteLine("Puudulik!");
+            }
+            else
+            {
+                Console.WriteLine("Vale väärtus!");
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+        }
+    }
+}
---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // programm kusib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvab "Suureparane!",
+            //'B', konsool kuvab "Vaga hea!".
+            //'C', konsool kuvab "Hea!",
+            //'D', konsool kuvab "Rahuldav!",
+            //'E', konsool kuvab "Kasin!",
+            //'F', konsool kuvab "Puudulik!",
+            // kui kasutaja siestab midagi muud, siis konsool kuvab "vale vaartus"
+
+            //kasitame Switch
+
+            Console.WriteLine("Sisesta oma tulemus:");
+            char userResult = Convert.ToChar(Console.ReadLine().ToUpper());
+
+            switch (userResult)
+            {
+                case 'A':
+                    Console.WriteLine("Suurepärane!");
+                    break;
+                case 'B':
+                    Console.WriteLine("Hea!");
+                    break;
+                case 'C':
+                    Console.WriteLine("Rahuldav!");
+                    break;
+                case 'D':
+                    Console.WriteLine("Rahuldav!");
+                    break;
+                case 'E':
+                    Console.WriteLine("Kasin!");
+                    break;
+                case 'F':
+                    Console.WriteLine("Puudulik!");
+                    break;
+
+                default:
+                    Console.WriteLine("Vale väärtus");
+                    break;
+
+            }
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
+
